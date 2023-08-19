abe3135e258a218335eeedeaaa00cfb63649c076
Honor13 <onury.karaduman@gmail.com>
2023-08-20 00:35:47 +0300

    2

--- app/src/main/java/com/example/proje/MainActivity.kt
+++ app/src/main/java/com/example/proje/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.proje
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.proje.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tasarim:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        tasarim = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setContentView(tasarim.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(tasarim.bottomNav,navHostFragment.navController)
     }
 }
