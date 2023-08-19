c7251d159f1a996342defab459ae50c75cd11e40
Honor13 <onury.karaduman@gmail.com>
2023-08-19 23:36:50 +0300

    2

---
+++ app/src/main/java/com/example/proje/ui/fragment/CuzdanFragment.kt
@@ -0,0 +1,17 @@
+package com.example.proje.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.proje.R
+
+class CuzdanFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cuzdan, container, false)
+    }
+
+}
